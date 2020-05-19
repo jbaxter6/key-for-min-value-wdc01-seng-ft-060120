cfc08e8a5c50cf0dbdd29478cd7c87d44872dea4
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-19 16:30:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(hash)
  small_key = nil
  
  hash.each do |key, value|
-   if small_value == value || value < small_value
+   if small_value == nil || value < small_value
      small_value = value
      small_key = key
     end
